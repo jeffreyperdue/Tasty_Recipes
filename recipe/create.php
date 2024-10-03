bbf0cac98bd9e74a68b717b775c97b2af694e587
NickM69 <93398980+NickM69@users.noreply.github.com>
2024-10-03 10:19:23 -0400

    added spacing

--- recipe/create.php
+++ recipe/create.php
@@ -14,22 +14,27 @@ session_start();
         <label>Title</label> <br>
         <input name="title" type="text" required />
       </div>
+      <br>
       <div>
-        <label>Cook Time</label>
+        <label>Cook Time (In minutes)</label>
         <input name="cooking_time" type="text" required />
       </div>
+      <br>
       <div>
         <label>Add image (optional)</label>
         <input name="image" type="file" accept="image/png, image/jpeg" />
       </div>
+      <br>
       <div>
         <label>Recipe</label>
         <input name="recipe" type="text" maxLength="250" />
       </div>
+      <br>
       <div>
-        <label>Description</label>
-        <input name="description" type="text" maxLength="1000" />
+        <label>Instruction</label>
+        <input name="instruction" type="text" maxLength="1000" />
       </div>
+      <button type="submit">Post</button>
     </form>
   </body>
 </html>
