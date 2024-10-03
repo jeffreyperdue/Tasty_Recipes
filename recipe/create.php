3ee83209281959593b746e0fd6147bc37f75d055
NickM69 <93398980+NickM69@users.noreply.github.com>
2024-10-03 09:59:45 -0400

    Added basic user create fields

--- recipe/create.php
+++ recipe/create.php
@@ -1,1 +1,31 @@
-#placeholder
+<?php
+session_start();
+
+
+
+?>
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <form method="POST" action="">
+      <div>
+        <label>Title</label> <br>
+        <input name="title" type="text" required />
+      </div>
+      <div>
+        <label>Cook Time</label>
+        <input name="cTime" type="text" required />
+      </div>
+      <div>
+        <label>Add image (optional)</label>
+        <input name="image" type="file" accept="image/png, image/jpeg" />
+      </div>
+      <div>
+        <label>Description</label>
+        <input name="description" type="text" maxLength="1000" />
+      </div>
+    </form>
+  </body>
+</html>
