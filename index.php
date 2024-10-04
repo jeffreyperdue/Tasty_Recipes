c87dedbc41347b83c3f54dcb00fc895a23431ae6
Jeffrey <perduej7@mymail.nku.edu>
2024-10-03 21:08:11 -0400

    Added Bootstrap theme, created JSON, and recipe cards with dynamic content from JSON and updated recipe/index.php

--- index.php
+++ index.php
@@ -1,57 +1,106 @@
 <?php 
 require_once('Auth/auth.php');
 $error='';
-//if logged in, send to index.php in entity folder
-if(isLoggedIn()){
+
+// if logged in, redirect to recipe index
+if (isLoggedIn()) {
     header('location: recipe/index.php');
     die();
 }
 
-//check to see if form has been submitted with values
-if(count($_POST)>0){
-    
+// check if form is submitted
+if (count($_POST) > 0) {
     completeFields();
     correctFields();
 
-    //check for login credentials in csv file
-    if(strlen($error)==0){
-        $fp=fopen('users.csv.php','r');
-        while(!feof($fp)){
-            $line=fgets($fp);
-            $line=explode(';', $line);
-        
-            if(count($line)==2 && $_POST['email']==$line[0] && password_verify($_POST['password'],trim($line[1]))){
+    // check login credentials in CSV file
+    if (strlen($error) == 0) {
+        $fp = fopen('users.csv.php', 'r');
+        while (!feof($fp)) {
+            $line = fgets($fp);
+            $line = explode(';', $line);
+
+            if (count($line) == 2 && $_POST['email'] == $line[0] && password_verify($_POST['password'], trim($line[1]))) {
                 fclose($fp);
-                $_SESSION['email']=$line[0];
+                $_SESSION['email'] = $line[0];
                 header('location: recipe/index.php');
                 die();
             }
         }
         fclose($fp);
-        $error='user doesn\'t exist. please create an account';
+        $error = 'User doesn\'t exist. Please create an account.';
     }
 }
-
 ?>
 
-<html>
-    <head>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Tasty Recipes - Sign In</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php 
-        if(strlen($error)>0) echo $error;
-    ?>
-    <h1>Signin to Website</h1>
-    <form method="POST">
-        <label>Email</label><br />
-        <input name='email' type="email" required />
-        <br /><br />
-        <label>Password</label><br />
-        <input name='password' type="password" required/>
-        <br /><br />
-        <button type="submit">Sign In</button>
-        <br /><br />
-        <a href="Auth/signup.php">New to the site? Create an account here.</a>
-</form>
+    <!-- Header -->
+    <header>
+        <div class="header-area">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="index.php">
+                                    <img src="img/logo.png" alt="Logo">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <!-- Login Section -->
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title text-center">Sign In to Tasty Recipes</h3>
+                        <!-- Display error message if any -->
+                        <?php if (strlen($error) > 0): ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?php echo $error; ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <form method="POST">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" class="form-control" required>
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" class="form-control" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-4 w-100">Sign In</button>
+                        </form>
+                        
+                        <div class="text-center mt-3">
+                            <a href="Auth/signup.php">New to the site? Create an account here.</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- JS Scripts -->
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
