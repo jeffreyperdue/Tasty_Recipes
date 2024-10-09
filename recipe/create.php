dcd688b21afb7b6b982cb858edcce0134461c9bc
NickM69 <93398980+NickM69@users.noreply.github.com>
2024-10-09 10:01:27 -0400

    Almost finished create.php
    ingredients list not made into an array list yet

--- recipe/create.php
+++ recipe/create.php
@@ -1,7 +1,28 @@
 <?php
-session_start();
-
-
+if (count($_POST)>0){
+  $recipesJson = file_get_contents('recipes.json');
+  $recipes = json_decode($recipesJson, true);
+  $nextId = count($recipes) + 1;
+  $owner = 'Placeholder';
+  if ($_POST['image'] == null){
+    $_POST['image'] = '../img/No_Image_Available.jpg';
+  }
+  $newData = [
+    'id' => $nextId,
+    'title' => $_POST['title'],
+    'ingredients' => $_POST['ingredients'],
+    'instructions' => $_POST['instructions'],
+    'cooking_time' => $_POST['cooking_time'],
+    'category' => $_POST['category'],
+    'image' => $_POST['image'],
+    'owner' => $owner
+  ];
+  $recipes[]= $newData;
+  $recipes = json_encode($recipes, JSON_PRETTY_PRINT);
+  file_put_contents('recipes.json', $recipes);
+  header('location: index.php');
+}
+else{
 
 ?>
 <html>
@@ -16,25 +37,33 @@ session_start();
       </div>
       <br>
       <div>
-        <label>Cook Time (In minutes)</label>
+        <label>Ingredients</label> <br>
+        <input name="ingredients" type="text" required />
+      </div>
+      <br>
+      <div>
+        <label>Instruction</label> <br>
+        <input name="instructions" type="text" maxLength="1000" required/>
+      </div>
+      <br>
+      <div>
+        <label>Cook Time (In minutes)</label> <br>
         <input name="cooking_time" type="text" required />
       </div>
       <br>
       <div>
-        <label>Add image (optional)</label>
+        <label>Category</label> <br> <!--dropdown list-->
+        <input name="category" type="text" required>
+      </div>
+      <br>
+      <div>
+        <label>Add image (optional)</label> <br>
         <input name="image" type="file" accept="image/png, image/jpeg" />
       </div>
       <br>
-      <div>
-        <label>Recipe</label>
-        <input name="recipe" type="text" maxLength="250" />
-      </div>
-      <br>
-      <div>
-        <label>Instruction</label>
-        <input name="instruction" type="text" maxLength="1000" />
-      </div>
       <button type="submit">Post</button>
     </form>
   </body>
 </html>
+<?php } ?>
+
